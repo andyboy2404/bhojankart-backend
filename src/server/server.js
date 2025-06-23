445d08b7ef1fad3d26d1866da1a1a129e4710130
Anand Chourasiya <60183358+andyboy2404@users.noreply.github.com>
2025-06-23 20:53:09 +0530

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -9,6 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '..', '..', 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', '..', 'dist', 'index.html'));
+});
+
 // ✅ MySQL Connection
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
